89f9730f0d95202c3fa2de3dc8d5421c101449d3
Dusk-wings <pantkaran05@gmail.com>
2026-02-16 23:58:20 +0530

    feat: Add CardUpdateForm and DeleteContent components with basic structure

---
+++ frontend/src/components/cardUpdateForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CardUpdateForm() {
+    return (
+        <div>
+            <h1>Update Card</h1>
+        </div>
+    )
+}
+
+export default CardUpdateForm
---
+++ frontend/src/components/deleteContent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DeleteContent() {
+    return (
+        <div>
+            <h1>Delete Content</h1>
+        </div>
+    )
+}
+
+export default DeleteContent
