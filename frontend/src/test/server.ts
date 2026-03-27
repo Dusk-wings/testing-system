ba6e38f476a2710eb8fc228f9a9b87bbe6d47bd9
Dusk-wings <pantkaran05@gmail.com>
2026-03-28 02:37:35 +0530

    refactor: update mock server responses to use `HttpResponse.json` and refine Vitest commands.

--- frontend/src/test/server.ts
+++ frontend/src/test/server.ts
@@ -3,9 +3,9 @@ import { http, HttpResponse } from 'msw'
 
 export const server = setupServer(
     http.get(`${import.meta.env.VITE_BACKEND_PATH}/api/users`, () => {
-        return new HttpResponse({ message: "User not found" }, { status: 401 })
+        return HttpResponse.json({ message: "User not found" }, { status: 401 })
     }),
     http.post(`${import.meta.env.VITE_BACKEND_PATH}/api/users/refresh-token`, () => {
-        return new HttpResponse({ message: "Refresh token not found" }, { status: 401 })
+        return HttpResponse.json({ message: "Refresh token not found" }, { status: 401 })
     }),
 )
